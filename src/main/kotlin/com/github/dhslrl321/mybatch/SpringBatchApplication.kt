412a2e03f35c4dfe99f6819e5940b86239abe757
dhslrl321 <dhslrl321@gmail.com>
2023-11-04 19:42:36 +0900

    commit

---
+++ src/main/kotlin/com/github/dhslrl321/mybatch/SpringBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.github.dhslrl321.mybatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatchApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/dhslrl321/mybatch/SpringBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.dhslrl321.mybatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
